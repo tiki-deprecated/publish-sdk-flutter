46dc1cbb0423c4a9e2f8feaf5fa68bfc2f259068
Ricardo Gonçalves <ricardo@mytiki.com>
2022-10-05 11:58:06 -0300

    tiki dar sdk methos implementation in Flutter

--- example/integration_test/tiki_sdk_flutter_test.dart
+++ example/integration_test/tiki_sdk_flutter_test.dart
@@ -1,23 +1,70 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:tiki_sdk_dart/consent/consent_service.dart';
 import 'package:tiki_sdk_dart/tiki_sdk.dart';
 import 'package:tiki_sdk_flutter/tiki_sdk_flutter.dart';
 
 import 'package:tiki_sdk_flutter/utils/flutter_key_store.dart';
 
 void main() {
+  String apiKey = '';
+
   test('Import Dart TIKI SDK ', () async {
     TestWidgetsFlutterBinding.ensureInitialized();
     Database database = sqlite3.openInMemory();
     FlutterKeyStore keyStore = FlutterKeyStore();
-    String apiKey = '';
     await TikiSdk().init('flutter test', database, keyStore, apiKey);
     expect(1,1);
   });
   test('Initialize Flutter TIKI SDK', () async {
-    String apiKey = '';
     await TikiSdkFlutter().init('flutter test', apiKey);
     expect(1,1);
+  });
+  test('Assign Ownership', () async {
+    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    String ownershipId = await tikiSdk.assignOwnership('test', TikiSdkDataTypeEnum.point, ['email']);
+    expect(1,1);
+  });
+  test('give and revoke consent', () async {
+    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    String ownershipId = await tikiSdk
+        .assignOwnership('test1', TikiSdkDataTypeEnum.point, ['email']);
+    await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
+    ConsentModel? consentGiven = tikiSdk.getConsent('test1');
+    expect(consentGiven!.destination.uses[0], "*");
+    expect(consentGiven.destination.paths[0], "*");
+    await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.none());
+    consentGiven = tikiSdk.getConsent('test');
+    expect(consentGiven!.destination.uses.isEmpty, true);
+    expect(consentGiven.destination.paths.isEmpty, true);
+  });
+  test('expire consent', () async {
+    bool ok = false;
+    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    String ownershipId = await tikiSdk
+        .assignOwnership('test2', TikiSdkDataTypeEnum.point, ['email']);
+    await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
+    await tikiSdk.applyConsent(
+        'test2', const TikiSdkDestination.all(), () => ok = true);
+    expect(ok, true);
+    await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all(),
+        expiry: DateTime.now());
+    await tikiSdk.applyConsent(
+        'test2', const TikiSdkDestination.all(), () => ok = false);
+    expect(ok, false);
+  });
+  test('run a function based on user consent', () async {
+    bool ok = false;
+    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    String ownershipId = await tikiSdk
+        .assignOwnership('test', TikiSdkDataTypeEnum.point, ['email']);
+    await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
+    ConsentModel? consentGiven = tikiSdk.getConsent('test3');
+    expect(consentGiven!.destination.uses[0], "*");
+    expect(consentGiven.destination.paths[0], "*");
+    await tikiSdk.applyConsent(
+        'test3', const TikiSdkDestination.all(), () => ok = true);
+    expect(ok, true);
   });
 }
 
--- lib/tiki_sdk_flutter.dart
+++ lib/tiki_sdk_flutter.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
+import 'package:tiki_sdk_dart/consent/consent_service.dart';
 import 'package:tiki_sdk_dart/tiki_sdk.dart';
 import 'package:tiki_sdk_flutter/utils/flutter_key_store.dart';
 
@@ -16,6 +17,36 @@ class TikiSdkFlutter {
     tikiSdkDart = await TikiSdk().init(origin, database, keyStore, apiKey, id: id);
     return this;
   }
+
+  /// Assign ownership to a given [source] : data point, pool, or stream.
+  /// [types] describe the various types of data represented by
+  /// the referenced data. Optionally, the [origin] can be overridden
+  /// for the specific ownership grant.
+  Future<String> assignOwnership(
+      String source, TikiSdkDataTypeEnum type, List<String> contains,
+      {String? origin}) async => await tikiSdkDart.assignOwnership(source, type, contains);
+
+  /// Gets latest consent
+  ConsentModel? getConsent(String source, {String? origin}) => tikiSdkDart.getConsent(source);
+
+  /// Modify consent for [data]. Ownership must be granted before
+  /// modifying consent. Consent is applied on an explicit only basis.
+  /// Meaning all requests will be denied by default unless the
+  /// destination is explicitly defined in [destinations].
+  /// A blank list of [TikiSdkDestination.uses] or [TikiSdkDestination.paths]
+  /// means revoked consent.
+  Future<ConsentModel> modifyConsent(
+      String ownershipId, TikiSdkDestination destination,
+      {String? about, String? reward, DateTime? expiry}) async =>
+        await modifyConsent(ownershipId, destination, about: about, expiry: expiry);
+
+  /// Apply consent for [data] given a specific [destination].
+  /// If consent exists for the destination, [request] will be
+  /// executed. Else [onBlocked] is called.
+  Future<void> applyConsent(
+      String source, TikiSdkDestination destination, Function request,
+      {void Function(String)? onBlocked, String? origin}) async =>
+      await applyConsent(source, destination, request, onBlocked: onBlocked, origin: origin);
 
   // TODO: mutiple dbs for diferent users
   Future<Database> _openDb(String? id) async {
