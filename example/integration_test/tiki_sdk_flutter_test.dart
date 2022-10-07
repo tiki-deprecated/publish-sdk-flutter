78a617daec084e4f533a890608e69d86deabf915
Ricardo Gonçalves <ricardo@mytiki.com>
2022-10-07 06:46:57 -0300

    update tests

--- example/integration_test/tiki_sdk_flutter_test.dart
+++ example/integration_test/tiki_sdk_flutter_test.dart
@@ -1,32 +1,56 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:sqlite3/sqlite3.dart';
 import 'package:tiki_sdk_dart/consent/consent_service.dart';
+import 'package:tiki_sdk_dart/node/l0_storage.dart';
 import 'package:tiki_sdk_dart/tiki_sdk.dart';
+import 'package:tiki_sdk_dart/tiki_sdk_builder.dart';
 import 'package:tiki_sdk_flutter/tiki_sdk_flutter.dart';
+
+import 'package:tiki_sdk_flutter/tiki_sdk_flutter_builder.dart';
 
 import 'package:tiki_sdk_flutter/utils/flutter_key_store.dart';
 
-void main() {
-  String apiKey = '';
+import 'in_mem_storage.dart';
 
+void main() {
   test('Import Dart TIKI SDK ', () async {
     TestWidgetsFlutterBinding.ensureInitialized();
     Database database = sqlite3.openInMemory();
     FlutterKeyStore keyStore = FlutterKeyStore();
-    await TikiSdk().init('flutter test', database, keyStore, apiKey);
-    expect(1,1);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkBuilderStorage sdkBuilder = TikiSdkBuilderStorage('com.mytiki');
+    sdkBuilder.keyStorage = keyStore;
+    await sdkBuilder.loadPrimaryKey();
+    sdkBuilder.database = database;
+    sdkBuilder.l0Storage = l0storage;
+    await sdkBuilder.buildSdk();
+    expect(1, 1);
   });
+
   test('Initialize Flutter TIKI SDK', () async {
-    await TikiSdkFlutter().init('flutter test', apiKey);
-    expect(1,1);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkFlutterBuilder builder = TikiSdkFlutterBuilder('flutter test');
+    builder.l0storage = l0storage;
+    await builder.build();
+    expect(1, 1);
   });
+
   test('Assign Ownership', () async {
-    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
-    String ownershipId = await tikiSdk.assignOwnership('test', TikiSdkDataTypeEnum.point, ['email']);
-    expect(1,1);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkFlutterBuilder builder = TikiSdkFlutterBuilder('flutter test');
+    builder.l0storage = l0storage;
+    await builder.build();
+    TikiSdkFlutter tikiSdk = builder.tikiSdkFlutter;
+    await tikiSdk.assignOwnership('test', TikiSdkDataTypeEnum.point, ['email']);
+    expect(1, 1);
   });
+
   test('give and revoke consent', () async {
-    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkFlutterBuilder builder = TikiSdkFlutterBuilder('flutter test');
+    builder.l0storage = l0storage;
+    await builder.build();
+    TikiSdkFlutter tikiSdk = builder.tikiSdkFlutter;
     String ownershipId = await tikiSdk
         .assignOwnership('test1', TikiSdkDataTypeEnum.point, ['email']);
     await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
@@ -38,9 +62,14 @@ void main() {
     expect(consentGiven!.destination.uses.isEmpty, true);
     expect(consentGiven.destination.paths.isEmpty, true);
   });
+
   test('expire consent', () async {
     bool ok = false;
-    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkFlutterBuilder builder = TikiSdkFlutterBuilder('flutter test');
+    builder.l0storage = l0storage;
+    await builder.build();
+    TikiSdkFlutter tikiSdk = builder.tikiSdkFlutter;
     String ownershipId = await tikiSdk
         .assignOwnership('test2', TikiSdkDataTypeEnum.point, ['email']);
     await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
@@ -53,9 +82,14 @@ void main() {
         'test2', const TikiSdkDestination.all(), () => ok = false);
     expect(ok, false);
   });
+
   test('run a function based on user consent', () async {
     bool ok = false;
-    TikiSdkFlutter tikiSdk = await TikiSdkFlutter().init('flutter test', apiKey);
+    L0Storage l0storage = InMemL0Storage();
+    TikiSdkFlutterBuilder builder = TikiSdkFlutterBuilder('flutter test');
+    builder.l0storage = l0storage;
+    await builder.build();
+    TikiSdkFlutter tikiSdk = builder.tikiSdkFlutter;
     String ownershipId = await tikiSdk
         .assignOwnership('test', TikiSdkDataTypeEnum.point, ['email']);
     await tikiSdk.modifyConsent(ownershipId, const TikiSdkDestination.all());
@@ -67,5 +101,3 @@ void main() {
     expect(ok, true);
   });
 }
-
-
