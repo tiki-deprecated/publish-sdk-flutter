d58d55231f4c7fda4a44ba39c71e7c12b0291bb8
Ricardo Gonçalves <ricardo@mytiki.com>
2022-10-17 10:42:18 -0300

    split module and plugin packages

--- tiki_sdk_flutter_plugin/android/src/main/kotlin/com/mytiki/tiki_sdk_flutter_plugin/L0StorageInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.mytiki.tiki_sdk_flutter_plugin
-
-interface L0StorageInterface {
-    fun read(path: String) : ByteArray?
-    fun write(path: String, obj: ByteArray)
-    fun getAll(address: String) : Map<String, ByteArray>
-}
