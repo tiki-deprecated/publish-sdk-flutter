203f3f710245b6e8b5d41fe2c28299facfa3c824
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-02-10 16:15:12 -0300

    fix: request list widget
    closes #163

--- example/lib/widgets/request_list.dart
+++ example/lib/widgets/request_list.dart
@@ -1,22 +1,14 @@
-import 'package:example/requests/req_model.dart';
-import 'package:example/requests/service.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../destination/model.dart';
-import '../destination/service.dart';
+import '../request.dart';
 
-class RequestsLayoutList extends StatelessWidget {
-  const RequestsLayoutList({super.key});
+class RequestList extends StatelessWidget {
+  final List<Request> requests;
+
+  const RequestList(this.requests, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    RequestsService service =
-        Provider.of<RequestsService>(context, listen: true);
-    DestinationModel destination =
-        Provider.of<DestinationService>(context, listen: true).model;
-    List<RequestModel> requests = service.model.log;
-    service.controller.startTimer(context, destination);
     return Container(
         width: double.infinity,
         decoration: BoxDecoration(
