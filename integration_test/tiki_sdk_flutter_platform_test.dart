842e443e9abae215e4ba5addd730f0d2d5965e37
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2023-03-22 15:21:45 -0300

    fix: update guard test

--- integration_test/tiki_sdk_flutter_platform_test.dart
+++ integration_test/tiki_sdk_flutter_platform_test.dart
@@ -71,43 +71,42 @@ void main() {
       Map licenseMap = jsonDecode(jsonResponse)["license"];
       expect(1, 1);
     });
-  //
-  //   test('Guard', () async {
-  //     Completer<String> completer = Completer();
-  //     String requestId = 'build';
-  //     completers[requestId] = completer;
-  //     await channel.invokeMockMethod('build', {
-  //       "requestId": requestId,
-  //       "request": jsonEncode({"id": id, "publishingId": publishingId, "origin": origin})
-  //     });
-  //     await completer.future;
-  //     completer = Completer();
-  //     requestId = "license";
-  //     completers[requestId] = completer;
-  //     String? ptr = "source";
-  //     List uses = [{"usecases": ["support"]}];
-  //     await channel.invokeMockMethod('license', {
-  //       "requestId": requestId,
-  //       "request":
-  //         jsonEncode({
-  //           "terms":"path\/terms.md",
-  //           "tags":[{"titleTagEnum":"advertising_data"}],
-  //           "uses": uses,
-  //           "ptr": ptr,
-  //           "licenseDescription":"testing"})
-  //     });
-  //     await completer.future;
-  //     completer = Completer();
-  //     requestId = "guard";
-  //     completers[requestId] = completer;
-  //     await channel.invokeMockMethod('guard', {
-  //       "requestId": requestId,
-  //       "request": 	'{"ptr":"source","usecases":["support"],"destinations":[]}'
-  //     //"request": jsonEncode({"ptr": ptr, "usecases": uses[0]["usecases"]})
-  //     });
-  //     String jsonResponse = await completer.future;
-  //     expect(jsonDecode(jsonResponse)["success"], true);
-  //   });
+
+    test('Guard', () async {
+      Completer<String> completer = Completer();
+      String requestId = 'build';
+      completers[requestId] = completer;
+      await channel.invokeMockMethod('build', {
+        "requestId": requestId,
+        "request": jsonEncode({"id": id, "publishingId": publishingId, "origin": origin})
+      });
+      await completer.future;
+      completer = Completer();
+      requestId = "license";
+      completers[requestId] = completer;
+      String? ptr = "source";
+      await channel.invokeMockMethod('license', {
+        "requestId": requestId,
+        "request": jsonEncode({
+          "terms":"terms...",
+          "tags":["advertising_data"],
+          "uses":[{"destinations":[],"usecases":["support"]}],
+          "ptr":ptr,
+          "licenseDescription":
+          "Trade your IDFA (kind of like a serial # for your phone) for a discount.",
+          "expiry":1711040173000
+        })});
+      await completer.future;
+      completer = Completer();
+      requestId = "guard";
+      completers[requestId] = completer;
+      await channel.invokeMockMethod('guard', {
+        "requestId": requestId,
+        "request": 	'{"ptr":"source","usecases":["support"],"destinations":[]}'
+      });
+      String jsonResponse = await completer.future;
+      expect(jsonDecode(jsonResponse)["success"], true);
+    });
   });
 }
 
